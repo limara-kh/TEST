3c6f9d2c9d3617c2696e33de0cb2f70ed1536ce8
Limara <limarakhan2@icloud.com>
2025-07-30 12:54:20 +0500

    Create fetch.js

---
+++ fetch.js
@@ -0,0 +1,10 @@
+  <div class="quiz">
+        <h1 style="font-size: 60px;">Сұрақтар</h1>
+        <p>Уақыт секундомері: <span>8</span>с</p>
+        <div></div>
+        <button id="button1" style="font-size: 30px;">Жалған</button>
+        <button id="button2" style="font-size: 30px;">Ақиқат</button>
+    </div>
+    <script src="fetch.js"></script>
+</body>
+</html>
